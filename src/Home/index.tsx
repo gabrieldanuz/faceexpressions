0a94b3c172d185cfa8f7c55841f3b941ee4ed222
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2023-06-30 23:08:54 -0300

    Finish

---
+++ src/Home/index.tsx
@@ -0,0 +1,94 @@
+import { ImageSourcePropType, View } from 'react-native'
+import { Camera, CameraType, FaceDetectionResult } from 'expo-camera'
+
+import { styles } from './styles'
+import { useEffect, useState } from 'react'
+import * as FaceDetector from 'expo-face-detector'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle
+} from 'react-native-reanimated'
+
+import neutralImg from '../assets/neutral.png'
+import smilingImg from '../assets/smiling.png'
+import winkingImg from '../assets/winking.png'
+
+export function Home() {
+  const [faceDetected, setFaceDetected] = useState(false)
+  const [permission, requestPermission] = Camera.useCameraPermissions()
+  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralImg)
+
+  const faceValues = useSharedValue({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  })
+
+  function handleFacesDetected({ faces }: FaceDetectionResult) {
+    const face = faces[0] as any
+
+    if (face) {
+      const { size, origin } = face.bounds
+
+      faceValues.value = {
+        width: size.width,
+        height: size.height,
+        x: origin.x,
+        y: origin.y
+      }
+
+      setFaceDetected(true)
+
+      if (face.smilingProbability > 0.5) {
+        setEmoji(smilingImg)
+      } else if (
+        face.leftEyeOpenProbability > 0.5 &&
+        face.rightEyeOpenProbability < 0.5
+      ) {
+        setEmoji(winkingImg)
+      } else {
+        setEmoji(neutralImg)
+      }
+    } else {
+      setFaceDetected(false)
+    }
+  }
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    zIndex: 1,
+    width: faceValues.value.width,
+    height: faceValues.value.height,
+    transform: [
+      { translateX: faceValues.value.x },
+      { translateY: faceValues.value.y }
+    ]
+  }))
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
+  if (!permission?.granted) {
+    return
+  }
+
+  return (
+    <View style={styles.container}>
+      {faceDetected && <Animated.Image source={emoji} style={animatedStyle} />}
+      <Camera
+        style={styles.camera}
+        type={CameraType.front}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
+          minDetectionInterval: 100,
+          tracking: true
+        }}
+      />
+    </View>
+  )
+}
